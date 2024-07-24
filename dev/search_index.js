a7ed6096ece19684604534c963173801e10e4986
Documenter.jl <documenter@juliadocs.github.io>
2024-07-24 16:17:19 +0000

    build based on f89550c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PhyloTraits","category":"page"},{"location":"#PhyloTraits","page":"Home","title":"PhyloTraits","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PhyloTraits.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PhyloTraits]","category":"page"}]
+}
